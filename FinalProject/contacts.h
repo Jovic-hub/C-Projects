4dc73000a040c1b6c4ca31233e563401a50dff85
Jovic-hub <53789339+Jovic-hub@users.noreply.github.com>
2021-04-27 22:13:01 -0300

    Update contacts.h

--- FinalProject/contacts.h
+++ FinalProject/contacts.h
@@ -9,16 +9,6 @@
 #define CONTACTS_H_
 // ----------------------------------------------------------------------------
 
-//==============================================
-// Name:           Joao Victor Fernandes dos Santos
-// Student Number: 155858194
-// Email:          jvfernandes-dos-sant@myseneca.ca
-// Section:        NZZ
-// Date:           12/01/2020
-//==============================================
-// Assignment:     2
-// Milestone:      4
-//==============================================
 
 // +-------------------------------------------------+
 // | NOTE:  Copy/Paste your Assignment-2 Milestone-3 |
