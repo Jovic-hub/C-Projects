e247fc14aa9603887f363854c3d8a7f164b7c93e
Jovic-hub <53789339+Jovic-hub@users.noreply.github.com>
2021-04-27 22:12:14 -0300

    Update contacts.c

--- Workshop7/contacts.c
+++ Workshop7/contacts.c
@@ -1,13 +1,4 @@
-//==============================================
-// Name:           Joao Victor Fernandes dos Santos
-// Student Number: 155858194
-// Email:          jvfernandes-dos-sant@myseneca.ca
-// Section:        NZZ
-// Date:           20/11/2020
-//==============================================
-// Assignment:     2
-// Milestone:      2
-//==============================================
+
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
